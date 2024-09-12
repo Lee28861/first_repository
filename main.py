66c4671c133ea24adcd06de9cd840589ace4b36c
Lee28861 <ji28861@gmail.com>
2024-09-12 17:42:17 +0900

    fire ball

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def print_hello():
-    pass
+def add(x, y):
+    a = x + y
+    return a
+def print_KIT():
+    print("KIT")
 
 if __name__ == '__main__':
-    print_hello()
-    print("safsaf")
-
-print("hellooooow")    
+    print_KIT()
+    print(print_hello(1, 2))
